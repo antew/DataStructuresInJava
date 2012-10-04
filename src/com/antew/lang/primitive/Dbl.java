2f03f805f67ebe4caa3b86fbb1932b3b91cc385b
antew <antewcode@gmail.com>
2012-10-03 22:36:28 -0400

    Add toString methods to Int and Dbl.

--- src/com/antew/lang/primitive/Dbl.java
+++ src/com/antew/lang/primitive/Dbl.java
@@ -5,25 +5,30 @@ import com.antew.lang.Comparable;
 
 public class Dbl extends AbstractObject {
     protected double value;
-    
+
     public Dbl(double value) {
         this.value = value;
     }
-    
+
     public double doubleValue() {
         return value;
     }
-    
+
     @Override
     protected int compareTo(Comparable comp) {
         Dbl dbl = (Dbl) comp;
-        
+
         if (value < dbl.value)
             return -1;
         else if (value > dbl.value)
             return 1;
-        
+
         return 0;
+    }
+
+    @Override
+    public String toString() {
+        return Double.toString(value);
     }
 
 }
--- src/com/antew/lang/primitive/Int.java
+++ src/com/antew/lang/primitive/Int.java
@@ -6,26 +6,30 @@ import com.antew.lang.Comparable;
 public class Int extends AbstractObject {
 
     protected int value;
-    
+
     public Int(int value) {
         this.value = value;
     }
-    
+
     public int intValue() {
         return value;
     }
-    
+
     @Override
     protected int compareTo(Comparable comp) {
         Int integer = (Int) comp;
         long difference = (long) value - (long) integer.value;
-        
-        if (difference < 0) 
+
+        if (difference < 0)
             return -1;
-        else if (difference > 0) 
+        else if (difference > 0)
             return 1;
-        
+
         return 0;
     }
 
+    @Override
+    public String toString() {
+        return Integer.toString(value);
+    }
 }
